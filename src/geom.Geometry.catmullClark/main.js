3e211ddb31f165f747bb282c4d82899ca43e2ab9
Marcin Ignac <marcin.ignac@gmail.com>
2014-08-07 23:11:39 +0200

    Updated Geometry.catmullClark example with more shapes

--- src/geom.Geometry.catmullClark/main.js
+++ src/geom.Geometry.catmullClark/main.js
@@ -3,7 +3,7 @@ var glu = require('pex-glu');
 var materials = require('pex-materials');
 var color = require('pex-color');
 var geom = require('pex-geom');
-var gen = require('pex-geom-gen');
+var gen = require('pex-gen');
 var helpers = require('pex-helpers');
 
 var Cube = gen.Cube;
@@ -25,18 +25,32 @@ sys.Window.create({
     type: '3d'
   },
   init: function() {
-    var cube = new Box();
-    cube.computeEdges();
-    var cubeSub = cube.catmullClark().catmullClark().catmullClark().catmullClark();
+    var cube = new Box(0.5);
+    var cube2 = cube.catmullClark();
+    var cube3 = cube.catmullClark().catmullClark();
 
     this.scene = [];
 
-    this.scene.push(new Mesh(cubeSub, new SolidColor({ color: Color.Grey }), { lines: false }));
-    this.scene.push(new FaceHelper(cubeSub));
-    this.scene.push(new VertexHelper(cubeSub));
-    this.scene.push(new EdgeHelper(cubeSub));
-    //this.scene.push(new VertexNormalHelper(cube));
-    //this.scene.push(new FaceNormalHelper(cube));
+    var mesh = new Mesh(cube, new SolidColor({ color: Color.Grey }), { lines: false });
+    var vertexHelper = new VertexHelper(cube);
+    var edgeHelper = new EdgeHelper(cube);
+    mesh.position.x = vertexHelper.position.x = edgeHelper.position.x = -1;
+
+    var mesh2 = new Mesh(cube2, new SolidColor({ color: Color.Grey }), { lines: false });
+    var vertexHelper2 = new VertexHelper(cube2);
+    var edgeHelper2 = new EdgeHelper(cube2);
+    mesh2.position.x = vertexHelper2.position.x = edgeHelper2.position.x = 0;
+
+    var mesh3 = new Mesh(cube3, new SolidColor({ color: Color.Grey }), { lines: false });
+    var vertexHelper3 = new VertexHelper(cube3);
+    var edgeHelper3 = new EdgeHelper(cube3);
+    mesh3.position.x = vertexHelper3.position.x = edgeHelper3.position.x = 1;
+
+    this.scene = [
+      mesh, vertexHelper, edgeHelper,
+      mesh2, vertexHelper2, edgeHelper2,
+      mesh3, vertexHelper3, edgeHelper3
+    ];
 
     this.camera = new PerspectiveCamera(60, this.width / this.height);
     this.arcball = new Arcball(this, this.camera);
