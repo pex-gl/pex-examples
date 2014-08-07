f7542d0344ba5cf7bd56d1e6805517358b1d1449
Marcin Ignac <marcin.ignac@gmail.com>
2014-08-07 23:11:03 +0200

    Added platonic solids to Primitives example

--- src/gen.Primitives/main.js
+++ src/gen.Primitives/main.js
@@ -5,15 +5,21 @@ var color = require('pex-color');
 var gen = require('pex-gen');
 var geom = require('pex-geom');
 
-var Cube = gen.Cube;
 var Plane = gen.Plane;
+var Cube = gen.Cube;
 var Sphere = gen.Sphere;
+var Tetrahedron = gen.Tetrahedron;
+var Octahedron = gen.Octahedron;
+var Icosahedron = gen.Icosahedron;
+var Dodecahedron = gen.Dodecahedron;
+var HexSphere = gen.HexSphere;
 var Mesh = glu.Mesh;
 var SolidColor = materials.SolidColor;
 var PerspectiveCamera = glu.PerspectiveCamera;
 var Arcball = glu.Arcball;
 var Color = color.Color;
 var Rect = geom.Rect;
+var Vec3 = geom.Vec3;
 
 sys.Window.create({
   settings: {
@@ -22,11 +28,20 @@ sys.Window.create({
     type: '3d'
   },
   init: function() {
-    var shapes = [ new Plane(), new Plane(1,1,3,3), new Cube(), new Cube(1,1,1,3,3,3), new Sphere() ];
+    var shapes = [
+      new Plane(), new Plane(1,1,3,3),
+      new Cube(), new Cube(1,1,1,3,3,3),
+      new Tetrahedron(),
+      new Octahedron(),
+      new Icosahedron(),
+      new Dodecahedron().triangulate(), //we can't render 5 vertex faces otherwise
+      new Sphere(0.5, 8, 8), new Sphere(),
+      new HexSphere(0.5, 1).triangulate(), new HexSphere(0.5, 2).triangulate()
+    ];
 
     var objects = [];
 
-    var numRows = 2;
+    var numRows = 3;
     var shapesPerRow = 4;
 
     var W = this.width;
@@ -35,8 +50,7 @@ sys.Window.create({
     this.objects = shapes.map(function(g, i) {
       var x = i % shapesPerRow;
       var y = Math.floor(i/shapesPerRow);
-      
-      g.computeEdges();
+
       return {
         fillMesh: new Mesh(g, new SolidColor({ color: Color.Grey })),
         edgesMesh: new Mesh(g, new SolidColor({ color: Color.Yellow }), { lines: true }),
@@ -44,12 +58,14 @@ sys.Window.create({
       }
     })
     this.camera = new PerspectiveCamera(60, this.width / this.height);
-    this.arcball = new Arcball(this, this.camera, 2.5);
+    this.arcball = new Arcball(this, this.camera, 2);
+    this.arcball.setPosition(new Vec3(1.2, 0.7, 1.2))
   },
   draw: function() {
     glu.viewport(0, 0, this.width, this.height);
     glu.clearColorAndDepth(Color.Black);
     glu.enableDepthReadAndWrite(true);
+    glu.cullFace();
 
     this.objects.forEach(function(o) {
       glu.viewport(o.viewport.x, o.viewport.y, o.viewport.width, o.viewport.height);
