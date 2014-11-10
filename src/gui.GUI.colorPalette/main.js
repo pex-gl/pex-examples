8758906bcc13fe5443aa5a7c9c6b00e137b5ae60
Marcin Ignac <marcin.ignac@gmail.com>
2014-11-10 10:30:57 +0000

    Added cube to GUI.colorPalette

--- src/gui.GUI.colorPalette/main.js
+++ src/gui.GUI.colorPalette/main.js
@@ -5,10 +5,10 @@ var color = require('pex-color');
 var gen = require('pex-gen');
 var gui = require('pex-gui');
 
-var Box = gen.Box;
+var Cube = gen.Cube;
 var Dodecahedron = gen.Dodecahedron;
 var Mesh = glu.Mesh;
-var ShowNormals = materials.ShowNormals;
+var SolidColor = materials.SolidColor;
 var PerspectiveCamera = glu.PerspectiveCamera;
 var Arcball = glu.Arcball;
 var Color = color.Color;
@@ -21,15 +21,20 @@ sys.Window.create({
     type: '3d',
     fullscreen: sys.Platform.isBrowser
   },
-  bgColor: Color.Black,
+  bgColor: Color.fromRGB(0, 0, 0),
+  cubeColor: Color.fromRGB(1, 1, 1),
   init: function() {
     this.gui = new GUI(this);
     this.gui.addParam('BG color ', this, 'bgColor', { palette: '../../assets/palettes/example.png'})
-    //this.gui.addParam('BG color ', this, 'bgColor', { palette: '../../assets/palettes/rainbow.jpg'})
-    this.gui.addParam('BG color 2', this, 'bgColor')
+    this.gui.addParam('Cube Color', this, 'cubeColor', { palette: '../../assets/palettes/rainbow.jpg'})
+
+    this.camera = new PerspectiveCamera(60, this.width / this.height);
+    this.arcball = new Arcball(this, this.camera);
+    this.mesh = new Mesh(new Cube(), new SolidColor({ color: this.cubeColor }));
   },
   draw: function() {
     glu.clearColorAndDepth(this.bgColor);
+    this.mesh.draw(this.camera);
     this.gui.draw();
   }
 });
