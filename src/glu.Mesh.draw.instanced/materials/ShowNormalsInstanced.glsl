6ae3508ef29842d39ffa00de2b4a3399bf53083a
Marcin Ignac <marcin.ignac@gmail.com>
2014-12-15 12:40:29 +0000

    Fixed int bug that iOS is complaining about

--- src/glu.Mesh.draw.instanced/materials/ShowNormalsInstanced.glsl
+++ src/glu.Mesh.draw.instanced/materials/ShowNormalsInstanced.glsl
@@ -34,7 +34,12 @@ mat4 quatToMat4(vec4 q) {
   float yy = q.y * ys;
   float yz = q.y * zs;
   float zz = q.z * zs;
-  return transpose(mat4(1 - (yy + zz), xy - wz, xz + wy, 0, xy + wz, 1 - (xx + zz), yz - wx, 0, xz - wy, yz + wx, 1 - (xx + yy), 0, 0, 0, 0, 1));
+  return transpose(mat4(
+    1.0 - (yy + zz), xy - wz, xz + wy, 0.0,
+    xy + wz, 1.0 - (xx + zz), yz - wx, 0.0,
+    xz - wy, yz + wx, 1.0 - (xx + yy), 0.0,
+    0.0, 0.0, 0.0, 1.0
+  ));
 }
 
 varying vec4 vColor;
