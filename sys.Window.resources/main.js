e3ec88138ada7a0d4420dbd26682c0a6ff487c65
Marcin Ignac <marcin.ignac@gmail.com>
2015-07-06 16:43:43 +0100

    Added pre-init Window.resources loading example

---
+++ sys.Window.resources/main.js
@@ -0,0 +1,33 @@
+var Window = require('pex-sys/Window');
+var glslify = require('glslify-promise');
+
+Window.create({
+    settings: {
+        width: 1280,
+        height: 720,
+        type: '3d'
+    },
+    //TODO: it's tempting to include some kind of CWD (current working directory) support so we don't need to ASSETS_PATH = isBorwser ? 'assets' : __dirname + '/assets/'
+    //TODO: check if glslify still works if we don't use __dirname but ASSETS_PATH
+    resources: {
+        plaskIcon  : { image: '../assets/textures/plask.png' },
+        basicVert  : { glsl: glslify(__dirname + '/../assets/glsl/Basic.vert') },
+        basicFrag  : { glsl: glslify(__dirname + '/../assets/glsl/Basic.frag') },
+        data       : { json: '../assets/text/data.json' }
+    },
+    init: function() {
+        var res = this.getResources();
+        console.log('init');
+        console.log('plaskIcon', res.plaskIcon.width,  res.plaskIcon.height);
+        console.log('vert', res.basicVert);
+        console.log('frag', res.basicFrag);
+        console.log('color', res.data.color);
+
+        this.clearColor = res.data.color;
+    },
+    draw: function() {
+        var ctx = this.getContext();
+        ctx.setClearColor(this.clearColor[0], this.clearColor[1], this.clearColor[2], 1.0);
+        ctx.clear(ctx.COLOR_BIT);
+    }
+})
